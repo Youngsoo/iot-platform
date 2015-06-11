adca086970c88679c4527b64df408c0cf6e145b5
Youngsoo Choi <youngsoo.choi@lge.com>
2015-06-10 21:46:56 -0400

    Add terminal device class

--- experimental/level2/server/src/Protocol.java
+++ experimental/level2/server/src/Protocol.java
@@ -1,9 +1,23 @@
 import org.json.simple.*;
 
 public class Protocol {
-	public static String generateMessage(String msgType, String nodeId, String sensorType, String sensorValue)
+	public static String generateTerminalInitMsg(String userId)
 	{
-		String message = null;
+		JSONObject json = new JSONObject();
+		json.put("devicetype", "terminal");
+		json.put("userid", userId);
+		String message = json.toString();
+		return message;
+	}
+	
+	public static String generateCommandMsg(String nodeId, String sensorType, String sensorValue)
+	{
+		JSONObject json = new JSONObject();
+		json.put("msgtype", "command");
+		json.put("nodeid", nodeId);
+		json.put("sensortype", sensorType);
+		json.put("value", Integer.parseInt(sensorValue));
+		String message = json.toString();
 		return message;
 	}
 	
---
+++ experimental/level2/server/src/Terminal.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+
+public class Terminal {
+	
+	private static final int portNum = 550;
+
+	public static void main(String[] args) throws Exception {
+		Socket clientSocket;
+		
+		Scanner scanner = new Scanner(System.in);
+			
+		System.out.print("Enter the server IP: ");
+		String serverip = scanner.next();
+		
+		while(true) {
+	       try {
+	    	   System.out.println ("Trying to connect to " + serverip + " on port " + portNum);
+	    	   clientSocket = new Socket(serverip, portNum);
+	    	   break;
+			} catch (IOException e) {
+				System.err.println("Could not connect to " + serverip + " on port: " + portNum);
+			}
+	       
+	       Thread.sleep(3000);
+		}
+		
+		System.out.println("Connected to the server");
+		
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(clientSocket.getInputStream()));
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+		
+		while(true) {
+			String message = null;
+			message = Protocol.generateTerminalInitMsg("drabble");
+			out.println(message);
+			System.out.println(">> " + message);
+			
+			System.out.print("Enter node id: ");
+			String nodeId = scanner.next();
+			
+			System.out.print("Enter sensor type: ");
+			String sensorType = scanner.next();
+			
+			System.out.print("Enter sensor value: ");
+			String sensorValue = scanner.next();
+			
+			System.out.println("MESSAGE: " + message);
+			System.out.print("Send this command?[y/N] ");
+			String sendCmd = scanner.next();
+			if (sendCmd.equals("y")) {
+				message = Protocol.generateCommandMsg(nodeId, sensorType, sensorValue);
+				out.println(message);
+			}
+		}
+
+	}
+}
